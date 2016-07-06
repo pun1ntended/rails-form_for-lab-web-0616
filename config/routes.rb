d2f3e82fa04491ced1031a6a5d62acf33128a536
Steven McGrath <mcgrath.steven7@gmail.com>
2016-07-06 09:14:15 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,28 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  get 'students/new' => 'students#new', as: 'new_student'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get 'students' => 'students#index', as: 'students'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  post 'students' => 'students#create'
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  get 'students/:id/edit' => 'students#edit', as: 'edit_student'
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  patch 'students/:id' => 'students#update'
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  get 'students/:id' => 'students#show', as: 'student'
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+  get 'school_classes/new' => 'school_classes#new', as: 'new_school_class'
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  get 'school_classes' => 'school_classes#index', as: 'school_classes'
+
+  post 'school_classes' => 'school_classes#create'
+
+  get 'school_classes/:id/edit' => 'school_classes#edit', as: 'edit_school_class'
+
+  patch 'school_classes/:id' => 'school_classes#update'
+
+  get 'school_classes/:id' => 'school_classes#show', as: 'school_class'
+
 end
